6e10a275febf61cbcd95bc30bede60443c7d173c
Mike Liu <mike.liu.dev@gmail.com>
2023-08-25 13:28:30 -0400

    Configure typescript linter.
    Note that the tsconfig doesn't output typescript and only lints them.
    Transpilation will be done by esbuild bundling.

--- preact/tsc.sh
+++ preact/tsc.sh
@@ -4,11 +4,15 @@ TOP=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # verify required binaries are available
 for BINARY in deno; do
-    if ! type "$BINARY" > /dev/null 2>&1; then
+    if ! hash "$BINARY" 2> /dev/null; then
         echo "Required binary not found: $BINARY"
         exit 1
     fi
 done
 
-# start deno linter
-exec deno lint --watch
+exec deno run \
+    --allow-env \
+    --allow-read \
+    --allow-write \
+    'npm:typescript@5.1.6/tsc' \
+    --watch $@
